db3d023f3f9a6238c70af72460818588a61737cb
Haim Kastner <haim.kastner@gmail.com>
2021-09-16 22:19:59 +0300

    Upload front v3 own repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+} from '@angular/animations';
+import { SwUpdate } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+
+
+})
+export class AppComponent {
+
+  constructor() {
+ 
+  }
+}
